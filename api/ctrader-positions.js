642cec7e8f2d205db03f2f15754eff7fb715814f
VISIONTRADINGPROP <VISIONTRADINGPROP@GMAIL.COM>
2026-03-22 00:51:56 +0100

    Update ctrader-positions.js

--- api/ctrader-positions.js
+++ api/ctrader-positions.js
@@ -1,7 +1,7 @@
 // /api/ctrader-positions.js
 // Recupera posizioni aperte e storico trade da cTrader
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
